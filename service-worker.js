f38f18aba2ec1f4a9981c48d59400d2bfac4fdc8
amoesxavier <116120098+amoesxavier@users.noreply.github.com>
2025-08-07 18:03:29 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+
+self.addEventListener('install', function(e) {
+  console.log('Service Worker instalado');
+  e.waitUntil(
+    caches.open('controle-horas-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
